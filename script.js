84ef084ab30025e86926d087b3dcda1939e161af
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-23 08:58:35 -0700

    Add library func
    Added a function that prompts the user for info about a book and then
    creates that book object, storing it in the library array. Pulled the
    book method out of the constructor and put it into a prototype for
    proper code organization

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function Book(title, author, pages, status) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${status}`;
-    }
+    this.isRead = isRead;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
 console.log(theHobbit.info());
+
+let myLibrary = [];
+
+function addBookToLibrary() {
+    let title = prompt('What is the title of the book?');
+    let author = prompt('Who is the author of the book?');
+    let pages = prompt('How many pages are there?');
+    let isRead = prompt('Have you read this book?');
+
+    let newBook = new Book(title, author, pages, isRead)
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary();
+console.log(myLibrary);
