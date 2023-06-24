94044ff50d2179db2aaed39c0bfd4f7b3237ba9e
djns <130430285+DDJCS50@users.noreply.github.com>
2023-06-24 11:30:22 -0700

    Add sidebar form and form funcs
    Added sidebar elements to the html to hold the form for adding a book.
    Added styling to scss to make the sidebar look good and to make the form
    more visually appealing. Added func that unhides the form when
    the 'new book' button is clicked. Added form funcs that check if a book
    is already in the library and then displays the book if does not already
    exist there. Added form func that allows the user to insert a new book
    to the library

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.onPage = false;
 }
 
 Book.prototype.info = function() {
@@ -11,29 +12,57 @@ Book.prototype.info = function() {
 
 let myLibrary = [];
 
-function addBookToLibrary() {
-    let title = prompt('What is the title of the book?');
-    let author = prompt('Who is the author of the book?');
-    let pages = prompt('How many pages are there?');
-    let isRead = prompt('Have you read this book?');
-
-    let newBook = new Book(title, author, pages, isRead)
-    myLibrary.push(newBook);
+function addBookToLibrary(title, author, pages, isRead) {
+    let bookInLibrary = false;
+    myLibrary.forEach((book) => {
+        if (title == book.title) {
+            bookInLibrary = true;
+        }
+    });
+    if (bookInLibrary == false) {
+        let newBook = new Book(title, author, pages, isRead)
+        myLibrary.push(newBook);
+    }
 }
-
-addBookToLibrary();
 
 function displayBooks() {
     myLibrary.forEach((book) => {
-        let displayBox = document.querySelector('#cardBox');
-        let newBookCard = document.createElement('div');
-        newBookCard.innerText = `Title: ${book.title} 
-                                By: ${book.author} 
-                                Pages: ${book.pages} 
-                                Has Been Read?: ${book.isRead}`;
-        newBookCard.setAttribute('class', 'card');
-        displayBox.appendChild(newBookCard);
+        if (book.onPage == false) {
+            let displayBox = document.querySelector('#cardBox');
+            let newBookCard = document.createElement('div');
+            newBookCard.innerText = `Title: ${book.title} 
+                                    By: ${book.author} 
+                                    Pages: ${book.pages} 
+                                    Has Been Read?: ${book.isRead}`;
+            newBookCard.setAttribute('class', 'card');
+            displayBox.appendChild(newBookCard);
+            book.onPage = true;
+        } else {
+            return;
+        }
     });
 }
 
-displayBooks();
+let bookButton = document.querySelector('#newBookButton');
+bookButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    let bookForm = document.querySelector('form');
+    bookForm.style.visibility = 'visible';
+    bookForm.style.display = 'flex';
+});
+
+let submitButton = document.querySelector('#submitButton');
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let title = document.querySelector('#title')
+    let author = document.querySelector('#author')
+    let pages = document.querySelector('#pages')
+    let read = document.querySelector('#readStatus')
+
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    displayBooks();
+    let bookForm = document.querySelector('form');
+    bookForm.style.visibility = 'hidden';
+    bookForm.style.display = 'none';
+});
